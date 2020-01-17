cfd9631e218f5835f9aef8802aff170c1edcb104
Michelle Morin <morin2m@gmail.com>
2020-01-17 10:05:55 -0800

    update front-end logic to include branching that outputs  different programming language recommendations for  different answers to question about technical interest

--- js/scripts.js
+++ js/scripts.js
@@ -8,14 +8,47 @@ $(document).ready(function() {
     var interest = $("input:radio[name=interest]:checked").val();
     var motivation = parseInt($("#motivation").val());
 
-
-
-
     if (name) {
       $(".name").text(name);
     } else {
       $(".name").text("there");
     }
 
+    var recommendation;
+    var userInterest;
+      if (interest === "UX") {
+        recommendation = "JavaScript";
+        userInterest = "user experience (UX) design";
+      } else if (interest === "database") {
+        recommendation = "Python";
+        userInterest = "database administration";
+      } else if (interest === "systems") {
+        recommendation = "C++";
+        userInterest = "computer systems";
+      } else if (interest === "BI") {
+        recommendation = "SQL (while not a development language, learning SQL may be helpful for the types of data analysis commonly used for busisness intelligence)";
+        userInterest = "business intelligence analytics";
+      } else if (interest === "programmer") {
+        recommendation = "C#";
+        userInterest = "software development";
+      }
+
+    var userMotivation;
+    if (motivation === 1) {
+      userMotivation = "";
+
+    } else if (motivation === 2) {
+      userMotivation = "";
+
+    } else if (motivation === 3) {
+      userMotivation = "";
+
+    } else if (motivation === 4) {
+      userMotivation = "";
+    }
+
+    $(".language").text(recommendation);
+    $(".interest").text(userInterest);
+    $(".motivation").text(userMotivation);
   });
 });
